a2c5c40344e1e7387fbf0de635254074ccdb344f
Demon1056 <102851685+Demon1056@users.noreply.github.com>
2022-09-02 01:26:53 +0300

    done first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+const body = document.querySelector('body')
+const buttonStart = document.querySelector('[data-start]')
+const buttonStop = document.querySelector('[data-stop]')
+let timer = null
+
+
+buttonStart.addEventListener ('click', (event)=>{
+    activeButton(event); 
+    inactiveButton(buttonStop)
+      timer= setInterval (onButtonStart, 1000); 
+}
+  )
+
+function onButtonStart (){
+    return body.style.backgroundColor = getRandomHexColor() 
+
+}
+buttonStop.addEventListener('click', onButtonStop)
+
+function onButtonStop (event){
+    clearInterval (timer)
+    activeButton(event)
+    inactiveButton(buttonStart);
+}
+function activeButton(button){button.target.setAttribute('disabled','true')
+}
+function inactiveButton(button){button.removeAttribute('disabled')}
+
 
